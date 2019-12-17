71149c3593882b50c65d8d14988e6fee76ab7d2a
Steve Alley <mail4muon@gmail.com>
2019-12-17 12:44:18 -0700

    Card display update
    Cards are now shown all together and on the 'table'.
    Celtic cross data now imported with JSON.

--- PlotCards.py
+++ PlotCards.py
@@ -1,33 +1,32 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Dec 15 13:39:50 2019
+Spyder Editor
 
-@author: Steve Alley
+This is a temporary script file.
 """
-
 
 import matplotlib.pyplot as plt
 from PIL import Image
 #import numpy as np
 
 
-fig_cc = plt.figure(figsize=(12,8), facecolor='g')
+fig_cc = plt.figure(figsize=(10,8), facecolor='g')
 
 for n in range(0, 16):
     n_axes = 'ax' + str(n)
 #    print(n_axes)
     n_axes =  fig_cc.add_subplot(4, 4, n+1)
     n_axes.axis('off')
-    skip = [10, 12, 14, 16]
+    skip = [8, 10, 12, 14, 16]
     
     if n not in skip:
         back = Image.open('./images/Original_1910_Back.jpg')
     
-        imgplot = plt.imshow(back)
+        plt.imshow(back)
+        
+        if n == 0:       
 
-ax0 = fig_cc.add_subplot(4, 4, 1)
-ax0.set_title('Significator')
-sign = Image.open('./images/Ace of Cups.jpg') 
-imgplot = plt.imshow(sign) 
-plt.show()
-
+            n_axes.set_title('Significator')
+            sign = Image.open('./images/Ace of Cups.jpg') 
+            plt.imshow(sign) 
+#plt.show()
